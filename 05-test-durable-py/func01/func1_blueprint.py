ddeae9b3bc2c3766d4224eea9557451b0ae208f9
bui2jp <chikutaku.reiwa.2020@gmail.com>
2024-06-16 11:30:31 +0900

    up

---
+++ 05-test-durable-py/func01/func1_blueprint.py
@@ -0,0 +1,37 @@
+import logging
+import time
+import uuid
+import azure.functions as func
+import azure.durable_functions as df
+
+bp = df.Blueprint()
+
+
+# An HTTP-Triggered Function with a Durable Functions Client binding
+@bp.route(route="orchestrators/{functionName}")
+@bp.durable_client_input(client_name="client")
+async def http_start(req: func.HttpRequest, client):
+    function_name = req.route_params.get('functionName')
+    instance_id = "o1-" + str(uuid.uuid4())
+    await client.start_new(function_name, instance_id)
+    response = client.create_check_status_response(req, instance_id)
+    return response
+
+
+# Orchestrator
+@bp.orchestration_trigger(context_name="context")
+def hello_o1(context):
+    result1 = yield context.call_activity("hello", "Seattle")
+    result2 = yield context.call_activity("hello", "Tokyo")
+    result3 = yield context.call_activity("hello", "London")
+
+    return [result1, result2, result3]
+
+
+# Activity
+@bp.activity_trigger(input_name="city")
+def hello(city: str):
+    logging.info(f"Saying hello to {city} start")
+    time.sleep(60)
+    logging.info(f"Saying hello to {city} end")
+    return f"Hello {city}"
---
+++ 05-test-durable-py/func02/func2_blueprint.py
@@ -0,0 +1,83 @@
+import logging
+import azure.functions as func
+import azure.durable_functions as df
+import numpy as np
+import time
+import socket
+import threading
+import os
+
+bp = df.Blueprint()
+
+
+def log_thread_info(function_name, dummy=None):
+    hostname = socket.gethostname()
+    thread_id = threading.get_ident()
+    logging.info(f"{function_name} start, hostname: {hostname}, thread ID: {thread_id}")
+
+
+# An HTTP-Triggered Function with a Durable Functions Client binding
+@bp.route(route="orchestrators2/{functionName}")
+@bp.durable_client_input(client_name="client")
+async def http_start2(req: func.HttpRequest, client):
+    function_name = req.route_params.get('functionName')
+    taskCount = int(req.params.get('taskCount', '100'))
+    log_thread_info(f"http_start2: taskCount {taskCount}")
+    instance_id = await client.start_new(function_name, None, taskCount)
+    response = client.create_check_status_response(req, instance_id)
+    return response
+
+
+# Orchestrator
+@bp.orchestration_trigger(context_name="context")
+def hello_o2(context):
+
+    # F1
+    taskCount = context.get_input()
+    logging.info(f"hello_o2 taskCount: {taskCount}")
+    all_work_batch = yield context.call_activity("F1", taskCount)
+    # logging.info(f"all_work_batch: {all_work_batch}")
+
+    # F2
+    parallel_tasks = []
+    # n分割してActivityを呼び出す
+    split_work_batches = np.array_split(all_work_batch, len(all_work_batch) // 10)
+    for works in split_work_batches:
+        parallel_tasks.append(context.call_activity("F2", works.tolist()))
+    # logging.info(f"parallel_tasks: {parallel_tasks}")
+    outputs = yield context.task_all(parallel_tasks)
+
+    # F3
+    result = yield context.call_activity("F3", outputs)
+    return result
+
+
+# Activity
+@bp.activity_trigger(input_name="taskCount")
+def F1(taskCount=None):
+    # logging.info("F1 start")
+    # target_task_count = int(os.getenv('TARGET_TASK_COUNT', 100))
+    log_thread_info(f"F1: taskCount {taskCount}")
+    # 数値の配列作成する
+    return [i for i in range(taskCount)]
+
+
+# Activity
+@bp.activity_trigger(input_name="input")
+def F2(input):
+    # 時間のかかる処理
+    for i in input:
+        log_thread_info(f"F2 input: {i}")
+        # logging.info(f"input: {i}")
+        time.sleep(1)
+
+    # logging.info("F2 end")
+    return input
+
+
+# Activity
+@bp.activity_trigger(input_name="input")
+def F3(input):
+    log_thread_info("F3")
+    logging.info("F3 start")
+    return input
